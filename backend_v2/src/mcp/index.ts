e2b6b92a49d6bc1d16ecc0ed8e1a566b0797aaa1
Divakar Jaiswal <divakarjaiswal707@gmail.com>
2025-07-05 04:16:15 +0530

    Added mcp server initial setup

--- backend_v2/src/mcp/index.ts
+++ backend_v2/src/mcp/index.ts
@@ -1,41 +1,51 @@
-import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { z } from "zod";
+import z from "zod";
 
 // Create an MCP server
 const server = new McpServer({
   name: "Email Agent",
-  version: "1.0.0"
+  version: "1.0.0",
+  capabilities:{
+    resources:{},
+    tools:{}
+  }
+}
+);
+
+
+
+const getReply = ()=>{
+
+}
+
+
+server.tool(
+  "get-reply",
+  "Get reply for this gmail thread",
+  {
+    input: z.string()
+  },
+  async({input})=>{
+    return{
+      content:[
+       { type:"text",
+        text: "how are you divakar"}
+      ]
+    }
+  }
+)
+
+
+
+async function main(){
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.log("mcp server connected ")
+
+}
+
+main().catch((error) => {
+  console.error("Fatal error in main():", error);
+  process.exit(1);
 });
-
-// Add an addition tool
-server.tool("add",
-  { a: z.number(), b: z.number() },
-  async ({ a, b }) => ({
-    content: [{ type: "text", text: String(a + b) }]
-  })
-);
-
-// Add a dynamic greeting resource
-server.resource(
-  "greeting",
-  new ResourceTemplate("greeting://{name}", { list: undefined }),
-  async (uri, { name }) => ({
-    contents: [{
-      uri: uri.href,
-      text: `Hello, ${name}!`
-    }]
-  })
-);
-
-// Start receiving messages on stdin and sending messages on stdout
-const transport = new StdioServerTransport();
-await server.connect(transport);
-
-
-
-/*
-server.resource - get endpoint - get info and all but no actions 
-server.tools - post endpoint - will end up taking actions 
-
-*/
